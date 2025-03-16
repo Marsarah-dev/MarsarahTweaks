4adc146cedca5c3d838d3f5bf97f6e9979c80e9f
Marius Corca <mariuscorca@gmail.com>
2025-03-16 16:58:20 +0100

    Split all config related code into ConfigManager

--- MarsarahTweaks/MarsarahTweaks.cs
+++ MarsarahTweaks/MarsarahTweaks.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using BepInEx;
-using BepInEx.Configuration;
+﻿using BepInEx;
 using HarmonyLib;
-using ServerSync;
 using UnityEngine;
 
 namespace MarsarahTweaks
@@ -20,81 +11,28 @@ namespace MarsarahTweaks
 		internal const string ModVersion = "0.1.0";
 		internal const string Author = "Marsarah";
 		public const string ModGUID = Author + "." + ModName;
-		public static string ConfigFileName = ModGUID + ".cfg";
-		public static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
 
 		private readonly Harmony harmony = new Harmony(ModGUID);
 
-		private static readonly ConfigSync configSync = new ServerSync.ConfigSync(ModGUID) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
-		private static ConfigEntry<bool> serverConfigLocked;
-		public static ConfigEntry<bool> testJumpEnabled;
-
 		void Awake()
 		{
-			serverConfigLocked = config("1 - Main Mod", "Lock Configuration", true, "If on, only server admins can change the configuration.");
-			_ = configSync.AddLockingConfigEntry<bool>(serverConfigLocked);
-			testJumpEnabled = config("2 - Test", "Test Jump", true, "Test stuff");
-
+			ConfigManager.Init(Config);
 			harmony.PatchAll();
-			SetupWatcher();
 		}
 
 		private void OnDestroy()
 		{
-			((BaseUnityPlugin)this).Config.Save();
-		}
-		private void SetupWatcher()
-		{
-			FileSystemWatcher watcher = new FileSystemWatcher(Paths.ConfigPath, ConfigFileName);
-			watcher.Changed += ReadConfigValues;
-			watcher.Created += ReadConfigValues;
-			watcher.Renamed += ReadConfigValues;
-			watcher.IncludeSubdirectories = true;
-			watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
-			watcher.EnableRaisingEvents = true;
+			Config.Save();
 		}
 
-		private void ReadConfigValues(object sender, FileSystemEventArgs e)
-		{
-			if (!File.Exists(ConfigFileFullPath))
-			{
-				return;
-			}
-			try
-			{
-				MLog("ReadConfigValues called");
-				((BaseUnityPlugin)this).Config.Reload();
-			}
-			catch
-			{
-				MLog("There was an issue loading " + ConfigFileName);
-			}
-		}
-
-		// Config Template =========================================================================
-		ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
-		{
-			ConfigDescription extendedDescription = new ConfigDescription(description.Description + (synchronizedSetting ? " [Synced with Server] " : " [Not Synced with Server] "), description.AcceptableValues, description.Tags);
-
-			ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
-
-			SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
-			syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
-
-			return configEntry;
-		}
-
-		ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
-
-		// Test Jump ===============================================================================
+		// Test Jump Patch ============================================================
 		[HarmonyPatch(typeof(Character), nameof(Character.Jump))]
 		class TestJump_Patch
 		{
-			static void Prefix (ref float ___m_jumpForce)
+			static void Prefix(ref float ___m_jumpForce)
 			{
-				if (testJumpEnabled.Value)
+				if (ConfigManager.testJumpEnabled.Value)
 				{
-					MLog("Initial jump force: " + ___m_jumpForce);
 					___m_jumpForce = 15;
 					MLog("Modified jump force: " + ___m_jumpForce);
 				}
@@ -106,7 +44,7 @@ namespace MarsarahTweaks
 			}
 		}
 
-		// Marsarah Tweaks Log =====================================================================
+		// Logger =====================================================================
 		public static void MLog(string log, bool header = false, bool footer = false)
 		{
 			if (header)
