15a7aceb98918cbc933a62822fc5da02cc6845a4
Marius Corca <mariuscorca@gmail.com>
2025-03-16 16:37:53 +0100

    Placed a test patch and config in main class. Server Sync works here

--- MarsarahTweaks/MarsarahTweaks.cs
+++ MarsarahTweaks/MarsarahTweaks.cs
@@ -6,7 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
+using ServerSync;
 using UnityEngine;
 
 namespace MarsarahTweaks
@@ -23,15 +25,23 @@ namespace MarsarahTweaks
 
 		private readonly Harmony harmony = new Harmony(ModGUID);
 
-		public static ConfigManager configManager;
+		private static readonly ConfigSync configSync = new ServerSync.ConfigSync(ModGUID) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+		private static ConfigEntry<bool> serverConfigLocked;
+		public static ConfigEntry<bool> testJumpEnabled;
 
 		void Awake()
 		{
-			configManager = new ConfigManager(this);
-			configManager.ReadConfigFile();
+			serverConfigLocked = config("1 - Main Mod", "Lock Configuration", true, "If on, only server admins can change the configuration.");
+			_ = configSync.AddLockingConfigEntry<bool>(serverConfigLocked);
+			testJumpEnabled = config("2 - Test", "Test Jump", true, "Test stuff");
 
 			harmony.PatchAll();
 			SetupWatcher();
+		}
+
+		private void OnDestroy()
+		{
+			((BaseUnityPlugin)this).Config.Save();
 		}
 		private void SetupWatcher()
 		{
@@ -52,17 +62,52 @@ namespace MarsarahTweaks
 			}
 			try
 			{
-				MTLog("ReadConfigValues called");
+				MLog("ReadConfigValues called");
 				((BaseUnityPlugin)this).Config.Reload();
 			}
 			catch
 			{
-				MTLog("There was an issue loading " + ConfigFileName);
+				MLog("There was an issue loading " + ConfigFileName);
+			}
+		}
+
+		// Config Template =========================================================================
+		ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
+		{
+			ConfigDescription extendedDescription = new ConfigDescription(description.Description + (synchronizedSetting ? " [Synced with Server] " : " [Not Synced with Server] "), description.AcceptableValues, description.Tags);
+
+			ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+
+			SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+			syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+			return configEntry;
+		}
+
+		ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+
+		// Test Jump ===============================================================================
+		[HarmonyPatch(typeof(Character), nameof(Character.Jump))]
+		class TestJump_Patch
+		{
+			static void Prefix (ref float ___m_jumpForce)
+			{
+				if (testJumpEnabled.Value)
+				{
+					MLog("Initial jump force: " + ___m_jumpForce);
+					___m_jumpForce = 15;
+					MLog("Modified jump force: " + ___m_jumpForce);
+				}
+				else
+				{
+					___m_jumpForce = 8;
+					MLog("Default jump force: " + ___m_jumpForce);
+				}
 			}
 		}
 
 		// Marsarah Tweaks Log =====================================================================
-		public static void MTLog(string log, bool header = false, bool footer = false)
+		public static void MLog(string log, bool header = false, bool footer = false)
 		{
 			if (header)
 			{
