dccb7ed170f41c82d4fa8979b5dbe1e46b3e3b3c
Marius Corca <mariuscorca@gmail.com>
2025-03-16 13:51:39 +0100

    Created initial Harmony Patch

--- MarsarahTweaks/MarsarahTweaks.cs
+++ MarsarahTweaks/MarsarahTweaks.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
 namespace MarsarahTweaks
 {
-    public class Class1
-    {
-    }
+	[BepInPlugin(ModGUID, ModName, ModVersion)]
+	public class MarsarahTweaks : BaseUnityPlugin
+	{
+		internal const string ModName = "MarsarahTweaks";
+		internal const string ModVersion = "0.1.0";
+		internal const string Author = "Marsarah";
+		public const string ModGUID = Author + "." + ModName;
+		public static string ConfigFileName = ModGUID + ".cfg";
+		public static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+		private readonly Harmony harmony = new Harmony(ModGUID);
+
+		void Awake()
+		{
+			harmony.PatchAll();
+		}
+	}
 }
