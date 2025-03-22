8ce99f94cbbc977095ff1e612645ea8e26b2b378
Marius Corca <mariuscorca@gmail.com>
2025-03-22 23:09:17 +0100

    Added Extra Armor Stats

--- MarsarahTweaks/ConfigManager.cs
+++ MarsarahTweaks/ConfigManager.cs
@@ -75,6 +75,7 @@ namespace MarsarahTweaks
 			public static readonly ConfigMetadata StatusEffectsModifications = new ConfigMetadata("5 - Shorter Wet Effect And Potion Cooldowns", "Wet effect and potions cooldown timers reduced");
 			//public static readonly ConfigMetadata WeaponStaminaModifications = new ConfigMetadata("6 - Reduced Weapon Stamina Costs", "Weapon stamina swing costs reduced");
 			public static readonly ConfigMetadata LessStaminaModifications = new ConfigMetadata("6 - Less Stamina Usage", "Stamina use of all actions is reduced by 15%");
+			public static readonly ConfigMetadata ExtraArmorStatsModifications = new ConfigMetadata("7 - Extra Armor Stats", "Heavy armor provides extra HP, light armor provides extra stamina, mage armor provides extra base eitr");
 
 			public static readonly ConfigMetadata LighterMetalWeight = new ConfigMetadata("1 - Lighter Metal Weight", "(Toggling requires client relog) All metal (ore and bars) weight decreased to 8");
 		}
@@ -97,6 +98,7 @@ namespace MarsarahTweaks
 		public static ConfigEntry<bool> shorterStatusEffectsEnabled;
 		//public static ConfigEntry<bool> reducedWeaponStaminaEnabled;
 		public static ConfigEntry<bool> lessStaminaUsageEnabled;
+		public static ConfigEntry<bool> extraArmorStatsEnabled;
 
 		public static ConfigEntry<bool> lighterMetalWeightEnabled;
 
@@ -124,6 +126,7 @@ namespace MarsarahTweaks
 			shorterStatusEffectsEnabled = CreateConfig(ConfigSections.Features, Configs.StatusEffectsModifications.Name, true, Configs.StatusEffectsModifications.Description);
 			//reducedWeaponStaminaEnabled = CreateConfig(ConfigSections.Features, Configs.WeaponStaminaModifications.Name, true, Configs.WeaponStaminaModifications.Description);
 			lessStaminaUsageEnabled = CreateConfig(ConfigSections.Features, Configs.LessStaminaModifications.Name, true, Configs.LessStaminaModifications.Description);
+			extraArmorStatsEnabled = CreateConfig(ConfigSections.Features, Configs.ExtraArmorStatsModifications.Name, true, Configs.ExtraArmorStatsModifications.Description);
 
 			// ===== QOL
 			lighterMetalWeightEnabled = CreateConfig(ConfigSections.QOL, Configs.LighterMetalWeight.Name, true, Configs.LighterMetalWeight.Description);
