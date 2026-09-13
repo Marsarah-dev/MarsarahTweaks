6661b8d7e1354b49b6fa98fa09d4050b26e555d8
Marius Corca <mariuscorca@gmail.com>
2026-09-13 10:46:09 +0200

    Updated Less Ashlands Enemies

--- MarsarahTweaks/Patches/Features/AshlandsEnemiesChanges.cs
+++ MarsarahTweaks/Patches/Features/AshlandsEnemiesChanges.cs
@@ -29,15 +29,14 @@ namespace MarsarahTweaks.Patches.Features
 		private static readonly Dictionary<string, (int maxSpawned, int groupMin, int groupMax, float spawnChance)> originalSpawnData = new Dictionary<string, (int maxSpawned, int groupMin, int groupMax, float spawnChance)>();
 		private static readonly Dictionary<string, (int? maxSpawned, int? groupMin, int? groupMax, float? spawnChance)> spawnAdjustments = new Dictionary<string, (int? maxSpawned, int? groupMin, int? groupMax, float? spawnChance)>()
 		{
-			{ "Fallen Valkyrie",			(null, null, null, 15f) },	// 1, 1, 1, 20
-			{ "Asksvin [DAY]",				(1, null, 2, 20f) },		// 2, 1, 3, 30
-			{ "Asksvin [NIGHT]",			(2, null, null, 35f) },		// 3, 1, 3, 45
-			{ "Volture",					(2, null, null, null) },	// 3, 1, 2, 20
-			{ "Charred Twitcher [DAY]",		(2, 1, 2, 35f) },			// 3, 2, 4, 40
-			{ "Charred Twitcher [NIGHT]",	(3, 2, 3, null) },			// 4, 3, 6, 45
-			{ "Charred Archer",				(2, null, null, 30f) },		// 4, 1, 2, 35
-			{ "Charred Melee",				(2, null, null, 30f) },		// 4, 1, 2, 35
-			{ "Lava Blob",					(1, null, 1, 20f) }			// 2, 1, 2, 25
+			{ "Asksvin [DAY]",              (1, null, 2, null) },		// 2, 1, 3, 20 - pre 1.0: 2, 1, 3, 30
+			{ "Asksvin [NIGHT]",            (2, null, null, null) },	// 3, 1, 3, 30 - pre 1.0: 3, 1, 3, 45
+			{ "Volture",                    (2, null, null, null) },	// 3, 1, 2, 15 - pre 1.0: 3, 1, 2, 20
+			{ "Charred Twitcher [DAY]",     (2, 1, 2, null) },			// 3, 2, 4, 20 - pre 1.0: 3, 2, 4, 40
+			{ "Charred Twitcher [NIGHT]",   (3, 2, 3, null) },			// 4, 3, 6, 40 - pre 1.0: 4, 3, 6, 45
+			{ "Charred Archer",             (2, null, null, null) },	// 4, 1, 2, 15 - pre 1.0: 4, 1, 2, 35
+			{ "Charred Melee",              (2, null, null, null) },	// 4, 1, 2, 15 - pre 1.0: 4, 1, 2, 35
+			{ "Lava Blob",                  (1, null, 1, null) }		// 2, 1, 2, 15 - pre 1.0: 2, 1, 2, 25
 		};
 
 		public static void UpdateAshlandsSpawns(SpawnSystem spawnSystem)
@@ -50,10 +49,25 @@ namespace MarsarahTweaks.Patches.Features
 
 				foreach (SpawnSystem.SpawnData spawner in spawnList.m_spawners)
 				{
+					/*if ((spawner.m_biome & Heightmap.Biome.AshLands) != 0)
+					{
+						string prefabName = spawner.m_prefab != null ? spawner.m_prefab.name : "NULL";
+
+						log.Info(
+							$"Spawner: {spawner.m_name} | " +
+							$"Prefab: {prefabName} | " +
+							$"Max Spawned: {spawner.m_maxSpawned} | " +
+							$"Group Min: {spawner.m_groupSizeMin} | " +
+							$"Group Max: {spawner.m_groupSizeMax} | " +
+							$"Spawn Chance: {spawner.m_spawnChance}%"
+						);
+					}*/
+
 					if (spawnAdjustments.TryGetValue(spawner.m_name, out var newValues))
 					{
+						string prefabName = spawner.m_prefab != null ? spawner.m_prefab.name : "NULL";
 						//log.Info($"| {spawner.m_name,-25} | {spawner.m_maxSpawned,-3} | {spawner.m_groupSizeMin,-9} | {spawner.m_groupSizeMax,-9} | {spawner.m_spawnChance,-6}% |");
-
+						
 						if (ConfigManager.LessAshlandsEnemiesEnabled.Value)
 						{
 							// Backup
